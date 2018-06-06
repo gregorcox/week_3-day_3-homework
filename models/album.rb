59a59a5d1dc1c8c1ade6bc0641e47358e21999a0
Gregor Cox <gregorcox@gmail.com>
2018-06-06 15:15:08 +0100

    initial commit

---
+++ models/album.rb
@@ -0,0 +1,35 @@
+require('pg')
+
+class Album
+
+  attr_accessor :title, :genre, :artist_id
+  attr_reader :id
+
+  def initialize(options)
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+    @id = options['id'].to_i if options['id']
+  end
+
+
+  def save()
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @artist_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM albums"
+    SqlRunner.run(sql)
+  end
+
+  def customer()
+    sql = "SELECT * FROM customers WHERE id = $1"
+    values = [@customer_id]
+    customer = SqlRunner.run(sql, values).first
+    return Customer.new(customer)
+  end
+
+end
